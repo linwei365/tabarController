952e3137d8c735980dfd7744e614a5c7cb508f4d
Lin Wei <linwei365@gmail.com>
2017-01-09 21:21:15 -0800

    Initial Commit

---
+++ StoryboardMockup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardMockup
+//
+//  Created by Lin Wei on 1/9/17.
+//  Copyright © 2017 Lin Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
